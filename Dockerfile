bbc589fcd59c302498e9a21dfa81ac09e0c9270f
Achilleas Buisman <accounts@abuisman.nl>
2022-11-04 21:55:59 +0100

    Add script for full_sync using pg_dump and pg_restore

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM postgres:14.5-bullseye
 
 COPY sync.sh /bin/sync.sh
+COPY full_sync.sh /bin/full_sync.sh
 
 CMD /bin/sync.sh
---
+++ full_sync.sh
@@ -0,0 +1,1 @@
+pg_dump $FROM_URL -Fc --no-acl --no-owner --verbose $EXTRA_DUMP_FLAGS | pg_restore -d "$TO_URL" --clean --if-exists --verbose
